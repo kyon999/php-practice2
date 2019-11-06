9d5e837078f1dcd2ff1a1b6592e1897f8e44ac85
kyon999 <ikuyana.na.shi@gmail.com>
2019-11-06 23:43:42 +0900

    トライアル課題途中

---
+++ data/practice/form.php
@@ -0,0 +1,57 @@
+<?php
+$user = array(
+    0 => array(
+        'name' => '社長',
+        'team' => '社長',
+        'pass' => '1234'
+    ),
+    1 => array(
+        'name' => '鈴木さん',
+        'team' => '営業チーム',
+        'pass' => '1111'
+    ),
+    2 => array(
+        'name' => '高橋さん',
+        'team' => '経理チーム',
+        'pass' => '2222'
+    ),
+    3 => array(
+        'name' => '山田さん',
+        'team' => '営業チーム',
+        'pass' => '3333'
+    ),
+    4 => array(
+        'name' => '中村さん',
+        'team' => '経理チーム',
+        'pass' => '4444'
+    ),
+    5 => array(
+        'name' => '山田さん',
+        'team' => '総務チーム',
+        'pass' => '5555'
+    ),
+    
+)
+?>
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+    <h1>ログイン</h1>
+    <form action="task.php" method="POST">
+    <label for="name">名前</label>
+    <select name="name">
+    <?php foreach($user as $var){ ?>
+        <option value="name"><?php echo $var['name'] ?></option>
+    <?php } ?>
+
+    </select>
+    <br>
+    <label for="pass">パスワード</label>
+    <input type="text" name="pass">
+    <br>
+    <input type="submit">
+    </form>
+</body>
+</html>
---
+++ data/practice/task.php
@@ -0,0 +1,15 @@
+<?php
+$name = $_POST['name'];
+$pass = $_POST['pass'];
+
+
+?>
+
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+    <h1>タスクページ</h1>
+</body>
+</html>
